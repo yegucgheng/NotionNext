05fb51cf63515b5329cbb453195baec07aecded0
Yguchengstomcat <146932425+yegucgheng@users.noreply.github.com>
2023-10-06 07:44:14 -0500

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,22 +5,22 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://nmylaw.com' },
+    { title: '访问文档中心获取更多帮助', url: 'https://nmylaw.com' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: '兴趣分享',
+  HERO_TITLE_2: '思维认知',
+  HERO_TITLE_3: 'NMYLAW.COM',
+  HERO_TITLE_4: '新内容',
+  HERO_TITLE_5: '多媒体内容',
+  HERO_TITLE_LINK: 'https://nmylaw.com',
 
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HERO_CATEGORY_1: { title: '兴趣分享', url: '/tag/兴趣分享' },
+  HERO_CATEGORY_2: { title: '专业文章', url: '/tag/专业文章' },
+  HERO_CATEGORY_3: { title: '生活记录', url: '/tag/生活记录' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HERO_RECOMMEND_POST_TAG: '推荐',
