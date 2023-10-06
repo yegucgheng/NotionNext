fbfbe6b2b4719b322939669eb362fa77bdf491ff
Yguchengstomcat <146932425+yegucgheng@users.noreply.github.com>
2023-10-06 12:50:10 -0500

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://nmylaw.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://nmylaw.com' }
+    { title: '欢迎来到我的网站', url: 'https://nmylaw.com' },
+    { title: '可以点我也可以向下滑', url: 'https://nmylaw.com' }
   ],
 
   // 英雄区(首页顶部大卡)
