371c92395476d98df55b99b7f7ea77daec66172d
Yguchengstomcat <146932425+yegucgheng@users.noreply.github.com>
2023-10-06 03:32:18 -0500

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,10 +20,10 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '聂明月的主页', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '分享、记录，欢迎关注', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://nmylaw.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'nmy, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
+  CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'niemingyuee@gmail.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
