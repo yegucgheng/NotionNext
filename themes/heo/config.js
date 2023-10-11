72199dd7a4a2cd116675d58dc17ba179d6307e66
Yguchengstomcat <146932425+yegucgheng@users.noreply.github.com>
2023-10-11 11:28:41 -0500

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -99,8 +99,8 @@ const CONFIG = {
   ],
 
   SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  SOCIAL_CARD_TITLE_1: '交流频道',
-  SOCIAL_CARD_TITLE_2: '感兴趣的话题与我交流',
+  SOCIAL_CARD_TITLE_1: '欢迎交流 🆔',
+  SOCIAL_CARD_TITLE_2: 'Thanks for reading',
   SOCIAL_CARD_TITLE_3: '微信: 131 2177 6999 ',
   SOCIAL_CARD_URL: 'https://nmylaw.com',
 
